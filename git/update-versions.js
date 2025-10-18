9fd3c0c6462173e3cc4f1834109f59e6decc42ad
markush97 <markus@hilabs.eu>
2025-10-18 23:09:34 +0000

    fix: fix version tag using wrong quotes

--- git/update-versions.js
+++ git/update-versions.js
@@ -20,7 +20,8 @@ const targets = [
   {
     type: 'ts',
     file: path.join(repoRoot, 'src', 'version.ts'),
-    render: v => `export const APP_VERSION = "${v}";\n`,
+    render: v => `export const APP_VERSION = '${v}'
+    ;\n`,
   },
   {
     type: 'pkg',
